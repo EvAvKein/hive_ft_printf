6a695ae9030c23d01205ad94b4ad35f1fb46de97
Eve Aviv Keinan <evavkein@gmail.com>
2024-11-16 21:04:11 +0200

    update header (per new approach)

--- ft_printf.h
+++ ft_printf.h
@@ -6,18 +6,20 @@
 /*   By: ekeinan <ekeinan@student.hive.fi>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/13 18:09:59 by ekeinan           #+#    #+#             */
-/*   Updated: 2024/11/14 16:49:59 by ekeinan          ###   ########.fr       */
+/*   Updated: 2024/11/16 20:58:31 by ekeinan          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef FT_PRINTF_H
 # define FT_PRINTF_H
 
-int	ft_printf(const char *, ...);
+// Print to terminal with a template and value conversions (simplified remake)
+int		ft_printf(const char *format, ...);
 
-typedef struct {
-	char	*error;
-	int		arg_count;
-} input_data;
+// Write signed number to terminal
+void    ft_putdec(long long n);
+
+// Write unsigned number to terminal
+void    ft_putudec(unsigned long long n);
 
 #endif
