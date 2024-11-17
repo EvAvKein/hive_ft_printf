ed13d25da8995ae9445545f1e9f6260ba3facd4a
Eve Aviv Keinan <evavkein@gmail.com>
2024-11-17 20:53:19 +0200

    ft_putdecimals progress

--- ft_putdecimals.c
+++ ft_putdecimals.c
@@ -6,27 +6,26 @@
 /*   By: ekeinan <ekeinan@student.hive.fi>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/16 20:27:38 by ekeinan           #+#    #+#             */
-/*   Updated: 2024/11/16 20:58:17 by ekeinan          ###   ########.fr       */
+/*   Updated: 2024/11/17 16:07:53 by ekeinan          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <unistd.h>
 
-static void	recursive_until_write_last_digit(int n)
+static void	recursive_until_write_last_digit(long long n)
 {
 	int	digit_char;
 
 	if (n > 9)
-		recursive_until_write_last_digit(n / 10, fd);
+		recursive_until_write_last_digit(n / 10);
 	digit_char = (n % 10) + '0';
 	write(1, &digit_char, 1);
 }
-
-void	ft_putudec(long long n)
+void	ft_putdec(long long n)
 {
 	if (n == -9223372036854775807)
 	{
-		write(fd, "-9223372036854775807", 20);
+		write(1, "-9223372036854775807", 20);
 		return ;
 	}
 	if (n < 0)
@@ -37,7 +36,16 @@ void	ft_putudec(long long n)
 	recursive_until_write_last_digit(n);
 }
 
-void	ft_putdec(unsigned long long n)
+static void	recursive_until_write_last_digit_u(unsigned long long n)
 {
-	ft_putudec(n);
+	int	digit_char;
+
+	if (n > 9)
+		recursive_until_write_last_digit(n / 10);
+	digit_char = (n % 10) + '0';
+	write(1, &digit_char, 1);
+}
+void	ft_putudec(unsigned long long n)
+{
+	recursive_until_write_last_digit_u(n);
 }
