66f9a5c157876b99de883446a954c21e360ad40c
Eve Aviv Keinan <evavkein@gmail.com>
2024-11-20 21:33:04 +0200

    update headerfile per helper remakes

--- ft_printf.h
+++ ft_printf.h
@@ -6,9 +6,12 @@
 /*   By: ekeinan <ekeinan@student.hive.fi>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/13 18:09:59 by ekeinan           #+#    #+#             */
-/*   Updated: 2024/11/17 17:40:49 by ekeinan          ###   ########.fr       */
+/*   Updated: 2024/11/20 20:12:40 by ekeinan          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
+
+#include <stdint.h>
+#include <stddef.h>
 
 #ifndef FT_PRINTF_H
 # define FT_PRINTF_H
@@ -16,16 +19,28 @@
 // Print to terminal with a template and value conversions (simplified remake)
 int		ft_printf(const char *format, ...);
 
-// Write signed number to terminal
-void    ft_putdec(long long n);
+// Increase print count if it's still a valid count, or set to invalid if new is
+void	increase_print_count(size_t increase, size_t *print_count);
 
-// Write unsigned number to terminal
-void    ft_putudec(unsigned long long n);
+// Print a character and increase print count
+void	print_char(char c, size_t *print_count);
 
-// Write unsigned int to terminal
-void	ft_putunbr_fd(unsigned int n, int fd)
+// Print a string and increase print count
+void	print_str(char *s, size_t *print_count);
 
-// Write hexadecimal from number (lowercase or uppercase)
-void	ft_puthex(long long n, int lowercase);
+// Print a pointer address and increase print count
+void	print_ptr(uintptr_t ptr_as_num, size_t *print_count);
+
+// Print an integer and increase print count
+void    print_int(int n, size_t *print_count);
+
+// Print an unsigned integer and increase print count
+void    print_uint(unsigned int n, size_t *print_count);
+
+// Print a lowercase hexadecimal by number and increase print count
+void	print_lowercase_hex(unsigned long long n, size_t *print_count);
+
+// Print a uppercase hexadecimal by number and increase print count
+void	print_uppercase_hex(unsigned long long n, size_t *print_count);
 
 #endif
