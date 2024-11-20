249cef74c0af19205f67edabb6cc6a01c398d8b5
Eve Aviv Keinan <evavkein@gmail.com>
2024-11-20 21:34:04 +0200

    test file updates

--- main.test.c
+++ main.test.c
@@ -6,31 +6,42 @@
 /*   By: ekeinan <ekeinan@student.hive.fi>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/17 15:50:06 by ekeinan           #+#    #+#             */
-/*   Updated: 2024/11/18 10:56:26 by ekeinan          ###   ########.fr       */
+/*   Updated: 2024/11/20 21:25:09 by ekeinan          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <stdio.h>
+#include "libft/libft.h"
 #include "ft_printf.h"
 
 int	main(void)
 {
-	char				*str = "string";
-	long long			deci = 23847223423;
-	unsigned long long	udeci = 23847223423;
+	char				*str = "some characters";
+	long long			digits = -2384423;
+	int					inte = 320742;
+	unsigned long long	udigits = 2384742423;
+	size_t				chars;
+	char 				format[] = "ft_printing:\n\nchar: %c, \nstring: %s, \npointer: %p, \ndigits: %d, \ninteger: %i, \nunsigned digits: %u, \nhexadecimal (lowercase): %x, \nhexadecimal (uppercase): %X\n";
+	
+	chars = ft_printf(
+		format, 
+		str[5],
+		str,
+		str,
+		digits,
+		inte,
+		udigits,
+		inte,
+		inte
+	);
+	
+	ft_printf("format len: %i, printed len: %i\n", ft_strlen(format), chars);
 
-	ft_printf("
-			ft_printing:\n
-			\n
-			char: %c, \n
-			string: %s, \n
-			pointer: %p, \n
-			decimal: %d, \n", 
-			&str[3],
-			str,
-			str,
-			deci,
-			udeci);
+	ft_printf("---------\n");
+
+	char				*printf = "ft_printing:\ninvalid %y conversion";
+
+	ft_printf(printf);
 
 	return (0);
 }
