4806db54ea47f301cf9d515083266789e3e522bf
Eve Aviv Keinan <evavkein@gmail.com>
2024-11-20 21:32:37 +0200

    create ptr conversion helper

---
+++ print_ptr.c
@@ -0,0 +1,27 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   print_ptr.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ekeinan <ekeinan@student.hive.fi>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/11/20 15:23:06 by ekeinan           #+#    #+#             */
+/*   Updated: 2024/11/20 21:27:31 by ekeinan          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <unistd.h>
+#include "ft_printf.h"
+
+void	print_ptr(uintptr_t ptr_as_num, size_t *print_count)
+{
+	if (!ptr_as_num)
+	{
+		increase_print_count(write(1, "(nil)", 5), print_count);
+		return ;
+	}
+	increase_print_count(write(1, "0x", 2), print_count);
+	if (*print_count < 0)
+		return;
+	print_lowercase_hex(ptr_as_num,  print_count);
+}
