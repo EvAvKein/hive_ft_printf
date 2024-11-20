fb3a6757cb4cfe50e0e8bae2685abc55b9c104b7
Eve Aviv Keinan <evavkein@gmail.com>
2024-11-20 21:31:30 +0200

    create print_count handler (for write-error handling)

---
+++ increase_print_count.c
@@ -0,0 +1,25 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   increase_print_count.c                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ekeinan <ekeinan@student.hive.fi>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/11/20 14:59:53 by ekeinan           #+#    #+#             */
+/*   Updated: 2024/11/20 19:34:03 by ekeinan          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stddef.h>
+#include <stdio.h>
+
+void	increase_print_count(size_t increase, size_t *print_count)
+{
+	if (increase < 0)
+	{
+		*print_count = -1;
+		return;
+	}
+	if (*print_count >= 0)
+		*print_count += increase;
+}
