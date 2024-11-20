b8400f3426c2519a7968b0246716db34457ad15e
Eve Aviv Keinan <evavkein@gmail.com>
2024-11-20 21:30:17 +0200

    hex remake

--- print_hex.c
+++ print_hex.c
@@ -1,38 +1,50 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_puthex.c                                        :+:      :+:    :+:   */
+/*   print_hex.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: ekeinan <ekeinan@student.hive.fi>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/17 17:16:39 by ekeinan           #+#    #+#             */
-/*   Updated: 2024/11/17 18:10:12 by ekeinan          ###   ########.fr       */
+/*   Updated: 2024/11/20 21:26:42 by ekeinan          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <unistd.h>
-#include <stdint.h>
 #include "ft_printf.h"
 
-static void recursive_until_write_last_hex(uintptr_t n, int lowercase)
+void	print_uppercase_hex(unsigned long long n, size_t *print_count)
 {
-	int chr = 63;
+	char	chr;
 
 	if (n > 15)
 	{
-		recursive_until_write_last_hex(n / 16, lowercase);
+		print_uppercase_hex(n / 16, print_count);
+		if (print_count < 0)
+			return ;
 		n %= 16;
 	}
 	if (n < 10)
 		chr = (n % 10) + '0';
-	else if (lowercase)
-		chr = (n % 10) + 'a';
 	else
 		chr = (n % 10) + 'A';
-	write(1, &chr, 1);
+	increase_print_count(write(1, &chr, 1), print_count);
 }
 
-void ft_puthex(uintptr_t n, int lowercase)
+void	print_lowercase_hex(unsigned long long n, size_t *print_count)
 {
-	recursive_until_write_last_hex(n, lowercase);	
+	char	chr;
+
+	if (n > 15)
+	{
+		print_lowercase_hex(n / 16, print_count);
+		if (print_count < 0)
+			return ;
+		n %= 16;
+	}
+	if (n < 10)
+		chr = (n % 10) + '0';
+	else
+		chr = (n % 10) + 'a';
+	increase_print_count(write(1, &chr, 1), print_count);
 }
