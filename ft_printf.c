b646b8d62fc5e414419b64e0b0872cc0d745d765
Eve Aviv Keinan <evavkein@gmail.com>
2024-11-20 21:33:25 +0200

    ft_printf.c overhaul

--- ft_printf.c
+++ ft_printf.c
@@ -6,7 +6,7 @@
 /*   By: ekeinan <ekeinan@student.hive.fi>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2224/11/14 15:00:24 by ekeinan           #+#    #+#             */
-/*   Updated: 2024/11/18 10:40:27 by ekeinan          ###   ########.fr       */
+/*   Updated: 2024/11/20 21:26:02 by ekeinan          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,59 +16,72 @@
 #include "libft/libft.h"
 #include "ft_printf.h"
 
-static int conv_match(const char *str, char *conv)
+static void convert(const char *str, va_list args, size_t *print_count)
 {
-	return (!ft_strncmp(str, conv, 2));
-}
-static void process_conversion(const char *str, va_list args)
-{
+	str++;
+
 	if (!str)
 		return ;
-	else if (conv_match(str, "%c"))
-		ft_putchar_fd(va_arg(args, int), 1);
-	else if (conv_match(str, "%s"))
-		ft_putstr_fd((char *) va_arg(args, int *), 1);
-	else if (conv_match(str, "%p"))
-	{
-		write(1, "0x", 2);
-		ft_puthex(va_arg(args, uintptr_t), 1);
-	}
-	else if (conv_match(str, "%d"))
-		ft_putnbr_fd(va_arg(args, int), 1);
-	else if (conv_match(str, "%i"))
-		ft_putnbr_fd(va_arg(args, int), 1);
-	else if (conv_match(str, "%u"))
-		ft_putunbr(va_arg(args, unsigned int), 1);
-	else if (conv_match(str, "%x"))
-		ft_puthex(va_arg(args, long long), 1);
-	else if (conv_match(str, "%X"))
-		ft_puthex(va_arg(args, long long), 0);
-	else if (conv_match(str, "%%"))
-		write(1, "%%", 2);
+	else if (*str == 'c')
+		print_char(va_arg(args, int), print_count);
+	else if (*str == 's')
+		print_str(va_arg(args, char *), print_count);
+	else if (*str == 'p')
+		print_ptr(va_arg(args, uintptr_t), print_count);
+	else if (*str == 'd')
+		print_int(va_arg(args, int), print_count);
+	else if (*str == 'i')
+		print_int(va_arg(args, int), print_count);
+	else if (*str == 'u')
+		print_uint(va_arg(args, unsigned int), print_count);
+	else if (*str == 'x')
+		print_lowercase_hex(va_arg(args, unsigned long long), print_count);
+	else if (*str == 'X')
+		print_uppercase_hex(va_arg(args, unsigned long long), print_count);
+	else if (*str == '%')
+		increase_print_count(write(1, "%", 1), print_count);
 	else
-		write(1, str, 2);
+		increase_print_count(write(1, --str, 2), print_count);
+}
+
+static size_t	segment_length(const char *s)
+{
+	char	*pointer_to_next_sep;
+
+	pointer_to_next_sep = ft_strchr(s, '%');
+	if (pointer_to_next_sep)
+		return pointer_to_next_sep - s;
+	else
+		return ft_strlen(s);
 }
 
 int	ft_printf(const char *format, ...)
 {
 	size_t	f_i;
 	va_list args;
+	size_t	curr_seg_len;
+	size_t	print_count;
 
 	va_start(args, format);
 
 	f_i = 0;
+	print_count = 0;
 	while (format[f_i])
 	{
+		if (print_count < 0)
+			return (print_count);
 		if (format[f_i] == '%' && format[f_i + 1])
 		{
-			process_conversion(&format[f_i], args);
+			convert(&format[f_i], args, &print_count);
 			f_i += 2;
 			continue ;
 		}
-		write(1, &format[f_i++], 1);
+		curr_seg_len = segment_length(&format[f_i]);
+		increase_print_count(write(1, &format[f_i], curr_seg_len), &print_count);
+		f_i += curr_seg_len;
 	}
 
 	va_end(args);
 
-	return (1);
+	return (print_count);
 }
